787be18281f6d14b570df9e5c0362ae83697b4e1
lexag <lexa.grebnets@gmail.com>
2024-07-23 00:02:29 +0200

    new version

--- pack/update.bat
+++ pack/update.bat
@@ -1,2 +1,1 @@
 cp .\map\map.png '.\pack\Kakan texturer\assets\minecraft\textures\painting\skeleton.png'
-Compress-Archive -Force -LiteralPath '\.pack\Kakan texturer\.' -DestinationPath ".\pack\Kakan texturer.zip"
